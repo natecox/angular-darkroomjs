e90ea86d89975aa45d80716887c2c2267a0d202a
Nathan Cox <akujin@akujin.com>
2015-04-22 17:39:54 -0700

    Added mechanism for external URL submission.
    * Added a new text input to the demo page.
    * Hooked input to angular callback for image submission.
    * Added CORS statement to angular-darkroom.
    
    references #2

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,10 +1,12 @@
-(function() {
+(function(fabric, angular) {
     'use strict';
 
     var AppCtrl = function($scope) {
         var _me = this;
         _me.image = '';
         _me.preview_image = '';
+
+        console.log(fabric);
 
         function load_new_image(file) {
             var oFReader = new FileReader();
@@ -19,7 +21,15 @@
         _me.image_changed = function(image) {
             _me.preview_image = image;
             $scope.$apply();
-        }
+        };
+
+        _me.upload_by_url = function() {
+            console.log('called', _me.image_url);
+            fabric.Image.fromURL(_me.image_url, function(img) {
+                console.log(img.toString());
+            })
+            _me.image = _me.image_url;
+        };
 
         $scope.$on('flow::fileAdded', function(event, $flow, flowFile) {
             load_new_image(flowFile.file);
@@ -31,4 +41,4 @@
     angular
         .module('ncDarkroomTest', ['flow', 'angular-darkroom'])
         .controller('AppCtrl', AppCtrl);
-})();
+})(fabric, angular);
